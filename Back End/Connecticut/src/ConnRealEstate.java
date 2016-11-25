d7a391304d03995a065829070514a09a0ef4756b
Sunil GC <sunigc0@gmail.com>
2016-11-24 23:02:09 -0700

    Works with 3 rdfs and writes into csv or json

--- Back End/Connecticut/src/ConnRealEstate.java
+++ Back End/Connecticut/src/ConnRealEstate.java
@@ -9,10 +9,8 @@ import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.util.FileManager;
 import org.apache.jena.rdf.model.*;
 
-
 public class ConnRealEstate {
     static String defaultNameSpace = "http://www.semanticweb.org/connecticut/real-estate#";
-
     static Model connt = null;
     static Model schema = null;
     InfModel inferredRelation = null;
@@ -38,18 +36,7 @@ public class ConnRealEstate {
         connecticutRealEstate.bindReasoner();
 
         System.out.println("\nRun on final combined rdf from 2 rdfs");
-        connecticutRealEstate.getPopulationTowns(connecticutRealEstate.inferredRelation);
-
-        //Fuseki part
-        /* System.out.println("Fuseki test");
-        QueryExecution qe = QueryExecutionFactory.sparqlService(
-                "http://localhost:3030/pop/query", "PREFIX ds:<http://data.ct.gov/resource/igy9-udjm/>\n" +
-                        "select DISTINCT ?x ?name where { ?x ds:municipality ?name}");
-        ResultSet results = qe.execSelect();
-        //ResultSetFormatter.out(System.out, results);
-        ResultSetFormatter.outputAsJSON(System.out, results);
-        qe.close();
-        */
+        connecticutRealEstate.getResults(connecticutRealEstate.inferredRelation);
 
     }
 
@@ -73,6 +60,12 @@ public class ConnRealEstate {
         Property prop = schema.createProperty("http://www.w3.org/2002/07/owl#equivalentClass");
         Resource obj = schema.createResource(defaultNameSpace + "town");
         schema.add(resource,prop,obj);
+
+        resource = schema.createResource(defaultNameSpace + "municipality");
+        prop = schema.createProperty("http://www.w3.org/2002/07/owl#equivalentClass");
+        obj = schema.createResource(defaultNameSpace + "name");
+        schema.add(resource,prop,obj);
+
     }
 
     private void bindReasoner()
@@ -82,47 +75,36 @@ public class ConnRealEstate {
         inferredRelation = ModelFactory.createInfModel(reasoner, connt);
     }
 
-
-
-    private void getPopulationTowns(Model model)
-    {
-        runQuery(" select DISTINCT ?x ?z ?name ?house ?p ?q where { ?x ds:municipality ?name ." +
-                "?x ds:total_housing_units ?house ." +
-                "?y ds:town ?name ." +
-                "?y ds:_2015_total ?z ." +
-                "?y ds:_2015_male_total ?p ." +
-                "?y ds:_2015_female_total ?q .}", model);  //add the query string
-
-        //Write to file as csv or json
+    private void getResults(Model model){
+        runQuery(" select DISTINCT ?name ?total_houses ?occupied_houses ?vacant_houses" +
+                " ?total_pop_2015 ?total_male_2015 ?total_female_2015 " +
+                " ?address ?list_year ?assessed_value ?sales_price ?sales_ratio where { " +
+                " ?ns1 ds:municipality ?name ;" +
+                "      ds:total_housing_units ?total_houses ;" +
+                "      ds:occupied_housing_units ?occupied_houses ;" +
+                "      ds:vacant_housing_units ?vacant_houses ." +
+                " ?ns2 ds:town ?name ;" +
+                "     ds:_2015_total ?total_pop_2015 ;" +
+                "     ds:_2015_male_total ?total_male_2015 ;" +
+                "     ds:_2015_female_total ?total_female_2015 ." +
+                " ?ns3 ds:name ?name ;" +
+                "      ds:address ?address ;" +
+                "      ds:listyear ?list_year ;" +
+                "      ds:assessedvalue ?assessed_value ;" +
+                "      ds:saleprice ?sales_price ;" +
+                "      ds:salesratio ?sales_ratio ." +
+                "}", model);  //add the query string
     }
 
     private void runQuery(String queryRequest, Model model) {
-
         StringBuffer queryStr = new StringBuffer();
         // Establish Prefixes
         queryStr.append("PREFIX ds:<http://data.ct.gov/resource/igy9-udjm/> ");
-
         //Now add query
         queryStr.append(queryRequest);
         Query query = QueryFactory.create(queryStr.toString());
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
-
         ResultSet response = qexec.execSelect();
-       /* while (response.hasNext()) {
-            QuerySolution soln = response.nextSolution();
-            RDFNode name = soln.get("?name");
-            RDFNode housing = soln.get("?house");
-            RDFNode population = soln.get("?z");
-            RDFNode malepopulation = soln.get("?p");
-            RDFNode femalepopulation = soln.get("?q");
-            if (name != null)
-                System.out.println(name.toString() +" has total housing of "+ housing.toString()+" and population of "
-                        +population.toString()+" with total male "+malepopulation.toString()+" and total female "
-                        +femalepopulation.toString() +"\n");
-            else
-                System.out.println("No Friends found!");
-        } */
-
         writeToFile(response);
     }
 
@@ -138,6 +120,8 @@ public class ConnRealEstate {
         catch (FileNotFoundException fnf){
             fnf.printStackTrace();
         }
+
+        //Dont do both, you can do only one operation at a time. Comment one of them which you dont want
         ResultSetFormatter.outputAsJSON(jsonp, response);
         //ResultSetFormatter.outputAsCSV(csvp, response);
     }
